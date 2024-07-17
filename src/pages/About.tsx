c3843b29b6bbdf65f53209f0d177b386f9ee6b1b
charlie b <charles.buckley@mobelux.com>
2024-07-17 07:17:42 -0700

    bio page working

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import client from '../sanityclient';
 import imageUrlBuilder from '@sanity/image-url';
 import { SanityImageSource } from '@sanity/image-url/lib/types/types';
-import Hero from "../components/Hero";
 import Title from "../components/Title";
 
 interface Biography {
